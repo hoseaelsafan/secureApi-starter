c8d5a23355d031e75c9cf1d61eb9700e68cba821
hoseaelsafan <hoseaelsafan@gmail.com>
2025-09-08 21:39:13 +0700

    initiate config package use for generate token, validasi token, hashing password

---
+++ src/main/java/com/dee/secure_api/config/JwtUtils.java
@@ -0,0 +1,70 @@
+package com.dee.secure_api.config;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import jakarta.annotation.PostConstruct;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expirationMs}")
+    private long expirationMs;
+
+    private Key key;
+
+    @PostConstruct
+    public void init(){
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(String username){
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expirationMs);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+    }
+
+    public String getUsernameFromToken(String token){
+        return getClaimsFromToken(token).getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try{
+            getClaimsFromToken(token);
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+    private Claims getClaimsFromToken(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean isTokenExpired(String token) {
+        Date expiration = getClaimsFromToken(token).getExpiration(); // JJWT: exp claim
+        return expiration.before(new Date()); // true if expiration date < now
+    }
+
+}
---
+++ src/main/java/com/dee/secure_api/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.dee.secure_api.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+import java.security.Security;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private  final AuthenticationConfiguration authconfig;
+
+    // Authentication manager (used in AuthController)
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception{
+        return authconfig.getAuthenticationManager();
+    }
+
+    // Password hashing
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
