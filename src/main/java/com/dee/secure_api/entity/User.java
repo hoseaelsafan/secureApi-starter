5c0b6f151c32f6c12c6dfb2cb79765cd561570d5
hoseaelsafan <hoseaelsafan@gmail.com>
2026-05-12 21:35:13 +0700

    Feat : add properties file and use in env profile

--- src/main/java/com/dee/secure_api/entity/User.java
+++ src/main/java/com/dee/secure_api/entity/User.java
@@ -20,6 +20,7 @@ public class User {
     private String username;
     private String password;
     private String email;
+    
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_roles",
